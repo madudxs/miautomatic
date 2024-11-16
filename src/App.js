2069baa936e84297bb87a3e902fdc6f4ec5b4198
madudxs <maria.dudacorrea@gmail.com>
2024-11-16 19:53:43 -0300

    Fix: interface

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { ThemeProvider } from '@mui/material/styles';
+import theme from './styles/theme';
+import Login from './pages/Login';
+import Cadastro from './pages/Cadastro';
+import Principal from './pages/Principal';
+import Configuracao from './pages/Configuracao';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/principal" element={<Principal />} />
+        <Route path="/configuracao" element={<Configuracao />} />
+        <Route path="/cadastro" element={<Cadastro />} />
+      </Routes>
+    </Router>
+    </ThemeProvider>
   );
-}
+};
 
 export default App;
